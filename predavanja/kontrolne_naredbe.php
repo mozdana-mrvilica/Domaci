135721c49979a3b2dccec8ae0dd2d52f4ddd2d86
Vlado Moskov <moskovvlado@yahoo.com>
2019-10-31 16:15:42 +0100

    Adds files to folder domaci_2

---
+++ predavanja/kontrolne_naredbe.php
@@ -0,0 +1,22 @@
+<?php
+    $a = 10;
+    $b = 20;
+
+    if($a == $b){
+        //
+    }else{
+        //
+    }
+    switch($a){
+        case 10;
+        echo "10";
+        break;
+
+        default:
+    }
+    $niz = [1,2,3,4,5];
+    $i = 0;
+    while($i < count($niz)){
+        echo $niz[$i++]
+    }
+?>
